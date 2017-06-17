7b7bc6c8e49aeb2ea788f389cee5ba262858ede3
newbenhd <newben.hd@gmail.com>
2017-06-16 23:40:09 -0700

    Added flow control in main screen.

--- app/src/main/java/com/example/newben/leaguestargram/MainActivity.java
+++ app/src/main/java/com/example/newben/leaguestargram/MainActivity.java
@@ -4,10 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 }
